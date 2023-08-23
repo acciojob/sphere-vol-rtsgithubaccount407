9832c533e20c84cba6dfab36d3357a6dbfde12fb
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-08-23 15:26:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 	var radius = document.getElementById('radius').value;
 	radius = Math.abs(radius);
 	var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-	
+		 volume = volume.toFixed(5);
 	 document.getElementById('volume').value = volume;
-	 volume = volume.toFixed(5);
+
 	return false;
 }
 
